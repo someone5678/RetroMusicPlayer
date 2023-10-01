b4ac432bc5254309398b512ed0ece149e6f6dfca
someone5678 <someone5678@users.noreply.github.com>
2023-10-01 14:58:55 +0900

    RetroMusicPlayer: Remove F-Droid variant
    Change-Id: I3ea243a1015dfb6f1c4c4a2ff25d63d5f81c8ba4

--- app/src/fdroid/java/code/name/monkey/retromusic/extensions/extensions.kt
+++
@@ -1,9 +0,0 @@
-@file:Suppress("UNUSED_PARAMETER", "unused")
-
-package code.name.monkey.retromusic.extensions
-
-import android.content.Context
-import android.view.Menu
-import androidx.fragment.app.FragmentActivity
-
-fun Context.setUpMediaRouteButton(menu: Menu) {}
